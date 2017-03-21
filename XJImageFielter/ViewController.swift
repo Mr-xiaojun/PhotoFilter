182091d8794322305f39c950b69a1a4b181060eb
byron_ma <byron_ma@ehsy.com>
2017-03-21 14:36:45 +0800

    Initial Commit

---
+++ XJImageFielter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XJImageFielter
+//
+//  Created by Byron_ma on 17/3/21.
+//  Copyright © 2017年 ehsy_it. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ XJImageFielterTests/XJImageFielterTests.swift
@@ -0,0 +1,36 @@
+//
+//  XJImageFielterTests.swift
+//  XJImageFielterTests
+//
+//  Created by Byron_ma on 17/3/21.
+//  Copyright © 2017年 ehsy_it. All rights reserved.
+//
+
+import XCTest
+@testable import XJImageFielter
+
+class XJImageFielterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
